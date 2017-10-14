a4878d4502a641c4b22655d465ffbf7fbfbadce6
Jacob <jacob@localhost.localdomain>
2017-10-14 18:11:34 +0300

    travis

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -22,6 +22,13 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+      // chrome setup for travis CI using chromium
+      Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: [' - no-sandbox']
+      }
+   },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
